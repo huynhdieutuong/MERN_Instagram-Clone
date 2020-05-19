110a8d5d249abef40e2924fcec8073ed51680845
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2020-05-19 15:01:16 +0700

    Login User

--- controllers/auth.js
+++ controllers/auth.js
@@ -114,3 +114,47 @@ exports.confirmationEmail = async (req, res) => {
     res.status(500).send('Server error');
   }
 };
+
+exports.login = async (req, res) => {
+  // Validate
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    // Check for user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).json({ msg: 'Invalid credentials' });
+    }
+
+    // Check for password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ msg: 'Invalid credentials' });
+    }
+
+    // Return jsonwebtoken
+    const payload = {
+      user: {
+        id: user.id,
+      },
+    };
+
+    jwt.sign(
+      payload,
+      config.get('jwtSecret'),
+      { expiresIn: 360000 },
+      (err, token) => {
+        if (err) throw err;
+        res.json({ token });
+      }
+    );
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+};
--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { check } = require('express-validator');
 
-const { register, confirmationEmail } = require('../controllers/auth');
+const { register, confirmationEmail, login } = require('../controllers/auth');
 
 // @route   POST api/auth/register
 // @desc    Register user
@@ -25,5 +25,20 @@ router.post(
 // @desc    Confirmation email
 // @access  Public
 router.get('/confirmation/:token', confirmationEmail);
+
+// @route   POST /api/auth/login
+// @desc    Login user
+// @access  Public
+router.post(
+  '/login',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  login
+);
 
 module.exports = router;

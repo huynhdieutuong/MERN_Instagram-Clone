aeb1d151d2b0816cfe72a458c08d7079b90f3951
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2020-05-20 13:34:53 +0700

    Mark as Read & Clear Notification

--- controllers/notifications.js
+++ controllers/notifications.js
@@ -13,3 +13,52 @@ exports.getNotifications = async (req, res) => {
     res.status(500).send('Server Error');
   }
 };
+
+exports.markRead = async (req, res) => {
+  try {
+    const notification = await Notification.findById(req.params.id);
+
+    if (!notification) {
+      return res.status(404).json({ msg: 'Notification not found' });
+    }
+
+    // Check user authorized
+    if (notification.owner.toString() !== req.user.id) {
+      return res.status(400).json({ msg: 'User not authorized' });
+    }
+
+    // Mark read or unread
+    notification.isRead = !notification.isRead;
+    await notification.save();
+
+    res.json(notification);
+  } catch (err) {
+    console.error(err.message);
+
+    res.status(500).send('Server Error');
+  }
+};
+
+exports.clearNotification = async (req, res) => {
+  try {
+    const notification = await Notification.findById(req.params.id);
+
+    if (!notification) {
+      return res.status(404).json({ msg: 'Notification not found' });
+    }
+
+    // Check user authorized
+    if (notification.owner.toString() !== req.user.id) {
+      return res.status(400).json({ msg: 'User not authorized' });
+    }
+
+    // Clear notification
+    await notification.remove();
+
+    res.json({ msg: 'Cleared notification' });
+  } catch (err) {
+    console.error(err.message);
+
+    res.status(500).send('Server Error');
+  }
+};
--- routes/notifications.js
+++ routes/notifications.js
@@ -2,12 +2,27 @@ const express = require('express');
 const router = express.Router();
 
 const { protect } = require('../middlewares/auth');
+const checkObjectId = require('../middlewares/checkObjectId');
 
-const { getNotifications } = require('../controllers/notifications');
+const {
+  getNotifications,
+  markRead,
+  clearNotification,
+} = require('../controllers/notifications');
 
 // @route   GET api/notifications
 // @desc    Get all user's notifications
 // @access  Private
 router.get('/', protect, getNotifications);
+
+// @route   PUT api/notifications/:id
+// @desc    Mark as Read & Unread
+// @access  Private
+router.put('/:id', protect, checkObjectId('id'), markRead);
+
+// @route   DELETE api/notifications/:id
+// @desc    Clear notification
+// @access  Private
+router.delete('/:id', protect, checkObjectId('id'), clearNotification);
 
 module.exports = router;
